2c8e91e61cb3337d45d6c66191732c83a79346e0
Jason Dagana <daganajason72@gmail.com>
2025-12-13 14:33:52 +0100

    added cors config, baseurl  and dotenv

--- index.js
+++ index.js
@@ -1,28 +1,25 @@
+require("dotenv").config();
 const express = require("express");
 const app = express();
-const PORT = 3000;
+const cors = require("cors")
 const projectRoutes = require("./routes/projectRoutes");
 const blogRoutes = require("./routes/blogRoutes");
-const Project = require("./utils/uploadProject");
+const PORT = process.env.PORT || 3000;
 
+const BASE_URL = process.env.BASE_URL || "/api"
+
+app.use(cors({
+    origin: "*"
+}))
 app.use(express.json())
 
 app.get("/", (req, res) => {
   console.log("This is the default route");
-  res.status(200).send("This is the default route");
+  res.status(200).send("API is running...");
 });
-const car = {
-  hmm: 3,
-  hem: 4,
-  hmr: 5,
-};
 
-const p = new Project(car, null);
-console.log(
-  p.isRequiredFieldSatisfied()
-);
-app.use("/projects", projectRoutes);
-app.use("/blog", blogRoutes);
+app.use(`${BASE_URL}/projects`, projectRoutes);
+app.use(`${BASE_URL}/blog`, blogRoutes);
 
 app.listen(PORT, () => {
   return console.log(`server listening at port ${PORT}`);
